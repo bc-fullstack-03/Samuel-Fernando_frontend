d693833a66e488240d8a7824bd61d88812423837
SamuelFST <guti.samuel2002@gmail.com>
2023-05-21 18:30:56 -0300

    feat: profileCard component and profile page

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,36 @@
+import { UserCircle } from '@phosphor-icons/react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../Button';
+import Heading from '../Heading';
+import Text from '../Text';
+
+function ProfileCard() {
+  const userProfile = JSON.parse(localStorage.getItem('userProfile') as string);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/');
+  }
+
+  return (
+    <div className='basis-5/6'>
+      <Heading className='border-b border-slate-400 mt-4'>
+        <Text className='font-extrabold ml-4' size='xlg'>Perfil</Text>
+        <div className='flex items-center pl-5 py-4'>
+          {userProfile.photoUri ?
+            <img src={userProfile.photoUri} className='w-14 h-14 rounded-full' />
+            : <UserCircle size={56} weight='light' />
+          }
+          <Text className='font-extrabold ml-5' size='xlg'>{userProfile.name}</Text>
+        </div>
+        <Button className='max-w-sm ml-5 mb-4 align-middle' onClick={handleLogout}>
+          Sair
+        </Button>
+      </Heading>
+    </div>
+  );
+}
+
+export default ProfileCard;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,12 @@
+import MainScreen from '../../components/MainScreen';
+import ProfileCard from '../../components/ProfileCard';
+
+function Profile() {
+  return (
+    <MainScreen>
+      <ProfileCard />
+    </MainScreen>
+  );
+}
+
+export default Profile;
