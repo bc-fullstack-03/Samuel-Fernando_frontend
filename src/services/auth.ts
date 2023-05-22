02a9d7fac3349058d2682019697542cd62a7171c
SamuelFST <guti.samuel2002@gmail.com>
2023-05-21 23:09:27 -0300

    feat: service to return a authorization object

---
+++ src/services/auth.ts
@@ -0,0 +1,13 @@
+function getAuthHeader() {
+  const token = localStorage.getItem('accessToken');
+
+  const authHeader = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  return authHeader;
+}
+
+export { getAuthHeader };
