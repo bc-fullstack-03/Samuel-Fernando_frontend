8f496b9dde742b6ccf2924dc1d9bd314691b1726
SamuelFST <guti.samuel2002@gmail.com>
2023-05-23 11:28:36 -0300

    feat: list of all posts of authenticated user on profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
 import MainScreen from '../../components/MainScreen';
+import PostCard from '../../components/PostCard';
 import ProfileCard from '../../components/ProfileCard';
+import Text from '../../components/Text';
+import { Post } from '../../models/Post';
+import api from '../../services/api';
+import { getAuthHeader } from '../../services/auth';
 
 function Profile() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    async function getMyPosts() {
+      try {
+        const { data } = await api.get('/post', getAuthHeader());
+        setPosts(data);
+      } catch (err) {
+        toast.error('Ocorreu um erro ao obter seus Posts');
+      }
+    }
+
+    getMyPosts();
+  }, []);
+
   return (
     <MainScreen>
-      <ProfileCard />
+      <div className='flex flex-col w-full basis-5/6'>
+        <ProfileCard />
+        <section className='mt-4'>
+          <Text className='font-extrabold ml-5' size='xlg'>Meus Posts</Text>
+          {posts.map((post) => {
+            return (
+              <PostCard post={post} key={post.id} />
+            );
+          })}
+        </section>
+      </div>
     </MainScreen>
   );
 }
