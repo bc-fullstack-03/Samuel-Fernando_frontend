9bb0c6ffccbabf02307b230e45a4d5f0b20efb9c
SamuelFST <guti.samuel2002@gmail.com>
2023-05-22 22:32:37 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import api from '../../services/api';
+import { getAuthHeader } from '../../services/auth';
+import Feed from '../../components/Feed';
+import MainScreen from '../../components/MainScreen';
+import { Post } from '../../models/Post';
+
+function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const { data } = await api.get(`/feed?page=${page}`, getAuthHeader());
+        setHasMore(data.pageableResponse.hasNext);
+        setPosts((posts) => [...posts, ...data.postResponse]);
+      } catch (err) {
+        toast.error('Ocorreu um erro ao obter o feed');
+      }
+    }
+
+    getPosts();
+  }, [page]);
+
+  const loadMorePosts = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <MainScreen>
+      <Feed
+        posts={posts}
+        loadMorePosts={loadMorePosts}
+        hasMorePosts={hasMore}
+      />
+    </MainScreen>
+  );
+}
+
+export default Home;
