46365158a351fdf8d09ba2e1ae08b384cc5e23d6
SamuelFST <guti.samuel2002@gmail.com>
2023-05-24 14:15:52 -0300

    feat: search by name on friendsList

--- src/components/FriendsList/index.tsx
+++ src/components/FriendsList/index.tsx
@@ -7,9 +7,12 @@ import FriendCard from '../FriendCard';
 import api from '../../services/api';
 import { getAuthHeader } from '../../services/auth';
 import { Profile } from '../../models/Profile';
+import { TextInput } from '../TextInput';
 
 function FriendsList() {
   const [profiles, setProfiles] = useState<Profile[]>([]);
+  const [searchResult, setSearchResult] = useState<Profile[]>([]);
+  const renderProfiles = searchResult.length > 0 ? searchResult : profiles;
 
   const userProfile = JSON.parse(localStorage.getItem('userProfile') as string);
 
@@ -26,14 +29,31 @@ function FriendsList() {
     getProfiles();
   }, []);
 
+  async function searchProfiles(search: string) {
+    try {
+      const response = await api.get(`/profile?name=${search}`, getAuthHeader());
+      setSearchResult(response.data);
+    } catch (err) {
+      toast.error('Ocorreu um erro ao buscar um usuário');
+    }
+  }
+
   return (
     <div className='basis-5/6'>
       <Heading className='mt-4'>
         <div className='ml-5'>
           <Text className='font-extrabold' size='xlg'>Amigos</Text>
+          <div className='mt-3 mr-5'>
+            <TextInput.Root>
+              <TextInput.Input
+                placeholder='Buscar por nome'
+                onChange={(event) => searchProfiles(event.target.value)}
+              />
+            </TextInput.Root>
+          </div>
         </div>
       </Heading>
-      {profiles && profiles.map((profile) => {
+      {renderProfiles.map((profile) => {
         return profile.idUser != userProfile.idUser && (
           <FriendCard profile={profile} key={profile.idUser} />
         );
